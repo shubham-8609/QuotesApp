3ec7b462dd12259c02dfd3d8ce20c76ebff9c97d
shubham-8609 <shubhamgupta8609@gmail.com>
2026-04-30 13:11:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quotesapp/Screens/QuoteListItem.kt
@@ -0,0 +1,140 @@
+package com.example.quotesapp.Screens
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.FormatQuote
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.SweepGradient
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun QuoteListItem() {
+
+    Card(elevation = CardDefaults.cardElevation(defaultElevation = 4.dp, pressedElevation = 8.dp)) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(14.dp)
+        ) {
+            Image(
+                imageVector = Icons.Filled.FormatQuote, contentDescription = "Quote Icon",
+                colorFilter = ColorFilter.tint(Color.White),
+                alignment = Alignment.TopStart,
+                modifier = Modifier
+                    .size(40.dp)
+                    .rotate(180f)
+                    .background(color = Color.Black)
+            )
+
+
+            Spacer(modifier = Modifier.padding(4.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = "Time is the most valuable thing a man can spend.",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+
+                Box(
+                    modifier = Modifier
+                        .background(color = Color(0xFFEEEEEE))
+                        .fillMaxWidth()
+                        .height(1.dp)
+                )
+                Text(
+                    text = "Codeleg",
+                    style = MaterialTheme.typography.bodySmall,
+                    fontWeight = FontWeight.Thin,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+
+
+            }
+
+
+        }
+
+    }
+
+}
+
+@Preview
+@Composable
+fun QuoteDetail() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.sweepGradient(listOf(Color(0xFFffffff), Color(0xFFE3E3E3)))
+            )
+
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(4.dp),
+            modifier = Modifier.padding(32.dp)
+        ) {
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 24.dp)
+            ) {
+
+                Image(
+                    imageVector = Icons.Filled.FormatQuote,
+                    modifier = Modifier
+                        .rotate(180f)
+                        .size(80.dp),
+                    contentDescription = "Quote Image"
+                )
+
+                Text(
+                    text = "Time is the most valuable thing a man can spend.",
+                    fontFamily = FontFamily.Serif,
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(Modifier.height(15.dp))
+
+                Text(
+                    text = "--codeleg",
+                    fontFamily = FontFamily.SansSerif,
+                    style = MaterialTheme.typography.bodySmall,
+
+                )
+
+            }
+
+        }
+    }
+
+}
